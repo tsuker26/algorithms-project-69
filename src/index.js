013335305e4603e9cea977aeb1bd279b0caeee2e
Tsuker <igorek888rane@gmail.com>
2024-09-16 16:19:52 +0300

    реализовал инвертируемый индекс

--- src/index.js
+++ src/index.js
@@ -44,42 +44,36 @@ function binarySearch(array, token) {
 
 function search(docs, query) {
   const queryWords = query.toLowerCase().split(/\s+/);
+  const index = {};
 
-  const countMatches = (text) => {
-    const textWords = text.toLowerCase().split(/\s+/);
-    let matches = 0;
+  docs.forEach((doc) => {
+    const textWords = doc.text.toLowerCase().split(/\s+/);
 
     // eslint-disable-next-line no-restricted-syntax
     for (const word of queryWords) {
       if (binarySearch(textWords, word)) {
-        // eslint-disable-next-line no-plusplus
-        matches++;
+        if (index[word]) {
+          index[word].push(doc.id);
+        } else {
+          index[word] = [doc.id];
+        }
       }
     }
+  });
 
-    return matches;
-  };
-
-  // eslint-disable-next-line array-callback-return
-  const result = docs.reduce((acc, cur) => {
-    const matches = countMatches(cur.text);
-
-    if (matches > 0) {
-      acc.push({ id: cur.id, matches });
-    }
-
-    return acc;
-  }, []);
-
-  return quickSort(result, (a, b) => (a.matches > b.matches ? -1 : 1)).map((el) => el.id);
+  return index;
 }
 
-// const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
-// const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
-// const doc3 = { id: 'doc3', text: "I'm your shooter." };
-// const docs = [doc1, doc2, doc3];
+// const doc1 = { id: 'doc1', text: 'some text' };
+// const doc2 = { id: 'doc2', text: 'some text too' };
+// const docs = [doc1, doc2];
 
-// const result = search(docs, 'shoot at me');
-// console.log(result); // ['doc2', 'doc1']
+// const result = search(docs, 'some text too');
+// console.log(result);
+// const index = {
+//   some: ['doc1', 'doc2'],
+//   text: ['doc1', 'doc2'],
+//   too: ['doc2'],
+// };
 
 export default search;
